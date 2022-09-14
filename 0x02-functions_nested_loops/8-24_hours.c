c9159fcd71c91232dba77a921205cec9930fb600
GFavour9 <gfredibe4@gmail.com>
2022-09-14 16:28:55 -0700

    commit for 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* jack_bauer - check description
+* Description: it prints every minute
+* of the day of jack bauer
+* Return: nothing
+*/
+void jack_bauer(void)
+{
+	int m;
+	int h;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+
+	}
+
+}

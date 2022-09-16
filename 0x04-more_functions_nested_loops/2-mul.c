3096b8a846cf23e98bfe4489d29de1d818ba1e0e
GFavour9 <gfredibe4@gmail.com>
2022-09-16 14:51:43 -0700

    commit for 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - check description
+* @a : first number parameter
+* @b : second number parameter
+* Description: multiplies two numbers passed to it
+* Return: the product of the numbers
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+
+}

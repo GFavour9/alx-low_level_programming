05a0b378e17ebd300bd338a295eb6b4558228ffe
GFavour9 <gfredibe4@gmail.com>
2022-09-19 18:31:44 -0700

    commit for 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* puts_half - check description
+* @str : string input parameter
+* Description: prints the bottom half of array
+* Return: nothing
+*/
+void puts_half(char *str)
+{
+	int num = 0;
+	int i = 0;
+
+	while (str[num] != '\0')
+	{
+		num++;
+	}
+	if ((num % 2) != 0)
+		i = (num - 1) / 2;
+	else
+		i = num / 2;
+
+	for (; i <= num; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+
+}

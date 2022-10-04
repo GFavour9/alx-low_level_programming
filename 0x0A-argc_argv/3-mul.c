04d8b03251dc89f3a2ce55078468b869a459f036
GFavour9 <gfredibe4@gmail.com>
2022-10-04 13:53:38 -0700

    commit for 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Write a program that multiplies two numbers.
+  * @argc: Argument count
+  * @argv: Argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	int i = 0, j = 0;
+
+	if (argc == 3)
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		printf("%d\n", i * j);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
